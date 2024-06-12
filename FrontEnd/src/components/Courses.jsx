2a27ed1d52781d4e3d4a7f66063a76ef6ec75be7
JagannathGariNavya <jnavyan12@gmail.com>
2024-06-12 16:48:14 +0530

    structure

---
+++ FrontEnd/src/components/Courses.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Courses = () => {
+  return (
+    <div>
+      <h2>Courses</h2>
+      {/* Add courses content here */}
+    </div>
+  );
+};
+
+export default Courses;
---
+++ FrontEnd/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+      {/* Add home content here */}
+    </div>
+  );
+};
+
+export default Home;
