9acca1affdad48131658de4cfb5db38394939922
shreya-kushwaha40 <shreyakushwaha40@gmail.com>
2024-06-13 14:15:41 +0530

    Subcription Page Completed

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter,  Routes, Route } from "react-router-dom";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider} from '@chakra-ui/react'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ChakraProvider>
